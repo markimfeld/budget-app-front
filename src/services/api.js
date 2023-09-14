4779907ec12f5f535dcc16035b7feb72a1f4c8bc
Marcos Imfeld <sebastianimfeld@gmail.com>
2023-09-13 23:33:33 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,3 @@
 export const BACKEND_BASE_URL = "https://budget-app-k4i2-dev.fl0.io";
+// export const BACKEND_BASE_URL = "http://localhost:3001";
 export const DOLAR_API_BASE_URL = "https://dolarapi.com/v1/dolares";
